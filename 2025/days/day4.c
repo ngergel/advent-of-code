6f51366e58b76683f9a1a9a0076abbca1268b4d5
Noah Gergel <n.gergel1@gmail.com>
2025-12-23 04:26:52 -0800

    Completed days 4/5, added inputs for the next few days as well.

--- 2025/days/day4.c
+++ 2025/days/day4.c
@@ -15,16 +15,49 @@
 
 #include "aoc.h"
 
+#define MAX_N 200
+
+
+int boundry_count(char grid[MAX_N][MAX_N], int x, int y) {
+    if (x < 0 || y < 0 || x >= MAX_N || y >= MAX_N) return 0;
+    
+    int cnt = 0;
+    if (x > 0 && grid[x - 1][y] == '@') cnt++;
+    if (y > 0 && grid[x][y - 1] == '@') cnt++;
+    if (x < MAX_N - 1 && grid[x + 1][y] == '@') cnt++;
+    if (y < MAX_N - 1 && grid[x][y + 1] == '@') cnt++;
+
+    if (x > 0 && y > 0 && grid[x - 1][y - 1] == '@') cnt++;
+    if (x > 0 && y < MAX_N - 1 && grid[x - 1][y + 1] == '@') cnt++;
+    if (x < MAX_N - 1 && y > 0 && grid[x + 1][y - 1] == '@') cnt++;
+    if (x < MAX_N - 1 && y < MAX_N - 1 && grid[x + 1][y + 1] == '@') cnt++;
+
+    return cnt;
+}
+
 
 int day4_p1(void) {
-    FILE *f = fopen(test_inp, "r");
+    FILE *f = fopen(day4_inp, "r");
 
     if (!f) {
         perror("fopen");
         return 1;
     }
 
-    printf("day4\n");
+    char grid[MAX_N][MAX_N];
+    int num_rows = 0, num_cols, movable_rolls = 0;
+
+    memset(grid, 0, sizeof grid);
+    while (fgets(grid[num_rows++], MAX_N, f));
+    num_cols = strlen(grid[0]);
+
+    for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+            if (grid[i][j] == '@' && boundry_count(grid, i, j) < 4) movable_rolls++;
+        }
+    }
+
+    printf("Part 1 movable rolls: %d\n", movable_rolls);
 
     fclose(f);
     return 0;
@@ -32,14 +65,36 @@ int day4_p1(void) {
 
 
 int day4_p2(void) {
-    FILE *f = fopen(test_inp, "r");
+    FILE *f = fopen(day4_inp, "r");
 
     if (!f) {
         perror("fopen");
         return 1;
     }
 
-    printf("day4\n");
+    char grid[MAX_N][MAX_N];
+    int num_rows = 0, num_cols, movable_rolls = 0;
+    bool moved = true;
+
+    memset(grid, 0, sizeof grid);
+    while (fgets(grid[num_rows++], MAX_N, f));
+    num_cols = strlen(grid[0]);
+
+    while (moved) {
+        moved = false;
+
+        for (int i = 0; i < num_rows; i++) {
+            for (int j = 0; j < num_cols; j++) {
+                if (grid[i][j] == '@' && boundry_count(grid, i, j) < 4) {
+                    movable_rolls++;
+                    grid[i][j] = '.';
+                    moved = true;
+                }
+            }
+        }
+    }
+
+    printf("Part 2 movable rolls: %d\n", movable_rolls);
 
     fclose(f);
     return 0;
--- 2025/days/day5.c
+++ 2025/days/day5.c
@@ -15,16 +15,80 @@
 
 #include "aoc.h"
 
+#define MAX_N 250
+#define MAX_LINE 50
+
+
+struct interval {
+    unsigned long long start, end;
+};
+
+
+bool in_interval(long long x, struct interval* i) {
+    return x >= i->start && x < i->end;
+}
+
+
+bool interval_lt(struct interval* x, struct interval* y) {
+    if (x->start < y->start) return true;
+    else if (x->start == y->start && x->end < y->end) return true;
+    return false;
+}
+
+
+void insertion_sort(struct interval arr[MAX_N], size_t n) {
+    for (int i = 1; i < n; i++) {
+        struct interval key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && interval_lt(&key, &arr[j])) arr[j + 1] = arr[j], j--;
+
+        arr[j + 1] = key;
+    }
+}
+
+
+unsigned long long max_ull(unsigned long long a, unsigned long long b) {
+    return a >= b ? a : b;
+}
+
 
 int day5_p1(void) {
-    FILE *f = fopen(test_inp, "r");
+    FILE *f = fopen(day5_inp, "r");
 
     if (!f) {
         perror("fopen");
         return 1;
     }
 
-    printf("day5\n");
+    struct interval arr[MAX_N];
+    char line[MAX_LINE];
+    size_t n = 0;
+
+    while (fgets(line, MAX_LINE, f)) {
+        if (strlen(line) == 1) break;
+
+        char *first_str = strtok(line, "-"), *second_str = strtok(NULL, "-");
+        unsigned long long start = strtoull(first_str, NULL, 10), end = strtoull(second_str, NULL, 10);
+
+        arr[n++] = (struct interval){start, end + 1};
+    }
+
+    insertion_sort(arr, n);
+
+    int fresh = 0;
+    while (fgets(line, MAX_LINE, f)) {
+        long long id = strtoll(line, NULL, 10);
+
+        for (int i = 0; i < n; i ++) {
+            if (in_interval(id, &arr[i])) {
+                fresh++;
+                break;
+            }
+        }
+    }
+
+    printf("Part 1 # of fresh ingredients: %d\n", fresh);
 
     fclose(f);
     return 0;
@@ -32,14 +96,41 @@ int day5_p1(void) {
 
 
 int day5_p2(void) {
-    FILE *f = fopen(test_inp, "r");
+    FILE *f = fopen(day5_inp, "r");
 
     if (!f) {
         perror("fopen");
         return 1;
     }
 
-    printf("day5\n");
+    struct interval arr[MAX_N];
+    char line[MAX_LINE];
+    size_t n = 0;
+
+    while (fgets(line, MAX_LINE, f)) {
+        if (strlen(line) == 1) break;
+
+        char *first_str = strtok(line, "-"), *second_str = strtok(NULL, "-");
+        unsigned long long start = strtoull(first_str, NULL, 10), end = strtoull(second_str, NULL, 10);
+
+        arr[n++] = (struct interval){start, end + 1};
+    }
+
+    insertion_sort(arr, n);
+
+    unsigned long long fresh = 0;
+    for (size_t i = 0; i < n; i++) {
+        unsigned long long s = arr[i].start, e = arr[i].end;
+        
+        while (i + 1 < n && arr[i + 1].start < arr[i].end) {
+            e = max_ull(e, max_ull(arr[i].end, arr[i + 1].end));
+            i++;
+        }
+
+        fresh += e - s;
+    }
+
+    printf("Part 2 # of fresh ingredients: %lld\n", fresh);
 
     fclose(f);
     return 0;
