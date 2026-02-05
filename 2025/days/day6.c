95b3180547522098dba1dba42d6601d01fb804de
Noah Gergel <n.gergel1@gmail.com>
2026-02-04 21:03:25 -0800

    Finished part 2 of day 6

--- 2025/days/day6.c
+++ 2025/days/day6.c
@@ -50,14 +50,14 @@ int day6_p1(void) {
     }
 
     for (size_t i = 0; i < num_cols; i++) {
-        unsigned long long sum = problems[num_rows - 1][i];
+        unsigned long long res = problems[num_rows - 1][i];
 
         for (size_t j = 0; j < num_rows - 1; j++) {
-            if (problems[num_rows - 1][i]) sum *= problems[j][i];
-            else sum += problems[j][i];
+            if (problems[num_rows - 1][i]) res *= problems[j][i];
+            else res += problems[j][i];
         }
 
-        total_sum += sum;
+        total_sum += res;
     }
 
     printf("Part 1 Homework Sum: %lld\n", total_sum);
@@ -67,15 +67,75 @@ int day6_p1(void) {
 }
 
 
+bool space_col(char problems[MAX_ROWS][MAX_COLS], int num_rows, int col) {
+    for (size_t i = 0; i < num_rows; i++) {
+        if (problems[i][col] != ' ') return false;
+    }
+
+    return true;
+}
+
+
+int col_to_int(char problems[MAX_ROWS][MAX_COLS], int num_rows, int col) {
+    char val[MAX_ROWS];
+    memset(val, 0, sizeof val);
+
+    for (size_t i = 0, pos = 0; i < num_rows - 1; i++) {
+        if (problems[i][col] == ' ') continue;
+        val[pos++] = problems[i][col];
+    }
+
+    return atoi(val);
+}
+
+
 int day6_p2(void) {
-    FILE *f = fopen(test_inp, "r");
+    FILE *f = fopen(day6_inp, "r");
 
     if (!f) {
         perror("fopen");
         return 1;
     }
 
-    printf("day6\n");
+    char problems[MAX_ROWS][MAX_COLS];
+    int num_rows = 0, num_cols = 0;
+    char line[MAX_LINE];
+    unsigned long long total_sum = 0;
+
+    memset(problems, 0, sizeof problems);
+    while (fgets(line, MAX_LINE, f)) {
+        int col = 0;
+        
+        while (line[col++] != '\n') {
+            problems[num_rows][col - 1] = line[col - 1];
+        }
+
+        num_cols = col > num_cols ? col : num_cols;
+        num_rows++;
+    }
+
+    int col = num_cols - 2;
+    while (col >= 0) {
+        if (space_col(problems, num_rows, col)) col--;
+
+        // Scan ahead to get the operator
+        int offset = 0;
+        while (col - offset >= 0 && !space_col(problems, num_rows, col - offset)) offset++;
+        if (col - offset == 0) offset++;
+
+        bool op = problems[num_rows - 1][col - offset + 1] == '*';
+        unsigned long long res = op;
+
+        for (size_t i = 0; i < offset; i++) {
+            if (op) res *= col_to_int(problems, num_rows, col - i);
+            else res += col_to_int(problems, num_rows, col - i);
+        }
+
+        total_sum += res;
+        col -= offset;
+    }
+
+    printf("Part 2 Homework Sum: %lld\n", total_sum);
 
     fclose(f);
     return 0;
